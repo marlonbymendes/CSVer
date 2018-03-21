ffb72f134c6e11d0bad900b68ef0b3a33d145e8b
Marlon Mendes <marlonciriatico@gmail.com>
2018-03-21 14:47:48 -0300

    Refactoring CSVer

--- csver.py
+++ csver.py
@@ -1,3 +1,4 @@
+# %load csver.py
 
 # %load app.py
 import numpy as np
@@ -6,88 +7,47 @@ import matplotlib
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+from IPython.display import display
+import string
+
 class CSVer:
     
     DATE_INPUT_FORMAT = '%Y-%m-%d %H:%M:%S'
     DATE_OUTPUT_FORMAT = '%m/%Y'
     DATE_GRAPH_FORMAT = DATE_OUTPUT_FORMAT
     
-    ITEMS_COLUMN = 'Item'
-    DATES_COLUMN = 'Data'
-    #VALUES_COLUMN = 'ValorUnitarioSolicitado'
-    VALUES_COLUMN = 'ValorUnitarioAprovado'
-    
-    def __init__(self, file_name, nrows = None):
-        self.csv = pd.read_csv(file_name, nrows = nrows)
+    def __init__(self, csv_names, nrows = None):
+        self.csvs = list(map(pd.read_csv, csv_names))
 
-    def get_first_n_rows(self, n):
-        return  self.csv.head(n)
-
-    def get_column(self, column_name):
-        return self.csv[column_name]
-
-    def get_distinct_rows(self, column_name):
-        distinct_rows = self.get_column(column_name).unique()
+    def get_distinct_rows(self, column_name, csv_pos = 0):
+        csv = self.csvs[csv_pos]
+        distinct_rows = csv[column_name].unique()
         return distinct_rows
 
-    def filter_column_by_value(self, column, value):
-        return self.csv[self.csv[column] == value]
+    def filter_column_by_value(self, column, value, csv_pos = 0):
+        csv = self.csvs[csv_pos]
+        return csv[csv[column] == value]
     
-    def get_date_axis_from_column(self, data_frame = None, column = 'Data'):
+    def get_date_axis_from_column(self, data_frame = None,
+                                  column = 'Data', csv_pos = 0):
         if data_frame is None:
-            data_frame = self.csv
+            data_frame = self.csvs[csv_pos]
 
         dates = data_frame[column].values
         dates_date_time = [datetime.strptime(d, CSVer.DATE_INPUT_FORMAT) for d in dates]
         dates_axis = matplotlib.dates.date2num(dates_date_time)
         return dates_axis
-
-    def plot_histogram(self, y_axis, x_label = '', y_label = '', title = ''):
-        fig = plt.figure()
-        ax = fig.add_subplot(1,1,1)
-        plt.setp(ax.get_xticklabels(), rotation=15)
-        plt.hist(y_axis, 50)
-        plt.ylabel(y_label)
-        plt.title(title)
-        plt.show()
     
-    def plot_y_axis_along_time(self, x_axis, y_axis, x_label = '', y_label = '', title = ''):
-        hfmt = matplotlib.dates.DateFormatter(CSVer.DATE_GRAPH_FORMAT)
-        fig = plt.figure()
-        ax = fig.add_subplot(1,2,1)
-        ax.xaxis.set_major_formatter(hfmt)
-        plt.setp(ax.get_xticklabels(), rotation=15)
-        plt.scatter(x_axis, y_axis, s = 50, c = 'blue', alpha = 0.25)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-        plt.title(title)
-
-        ax = fig.add_subplot(1,2,2)
-        ax.xaxis.set_major_formatter(hfmt)
-        plt.setp(ax.get_xticklabels(), rotation=15)
-        plt.semilogy(x_axis, y_axis, '.')
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-        plt.title(title)
-        plt.show()
-
-    
-    def plot_item_value_along_time(self, item):
-        rows = self.filter_column_by_value(CSVer.ITEMS_COLUMN, item)
-        x = csver.get_date_axis_from_column(data_frame = rows, column = CSVer.DATES_COLUMN)
-        y = rows[CSVer.VALUES_COLUMN].values
-        csver.plot_histogram(y, 'Data (dia/mes/ano)', 'Valor unitário solicitado (R$)', item)
-        print('Ploting item \"{}\"...'.format(item))
-    
-    def plot_random_item_value_along_time(self):
-        distinct_items = self.get_distinct_rows(CSVer.ITEMS_COLUMN)
+    def get_random_item(self, column, csv_pos = 0):
+        distinct_items = self.get_distinct_rows(column, csv_pos)
         item = np.random.choice(distinct_items)
-        self.plot_item_value_along_time(item)
-       
+        return item
+
 if __name__ == '__main__':
-    csv_file_name = 'APROVADO.csv'
-    csver = CSVer(file_name = csv_file_name)
-    dt = csver.csv
-    distinct = dt['Item'].unique()
-    while True:
-        csver.plot_random_item_value_along_time()
+    solicitado = 'solicitado_small.csv'
+    aprovado = 'aprovado_small.csv'
+    
+    l = [solicitado, aprovado]
+    csver = CSVer(l)
+    dt = csver.get_random_item('Item', 0)
+    display(dt)
