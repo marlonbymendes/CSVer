921ccf6f1611895fc72e0ae9a5c1c540378b8003
Marlon Mendes <marlonciriatico@gmail.com>
2018-03-22 09:29:06 -0300

    Changing histogram style

--- plotter.py
+++ plotter.py
@@ -1,10 +1,19 @@
+import matplotlib.pyplot as plt
+
+
 class Plotter:
 
-  def plot_histogram(self, y_axis, x_label = '', y_label = '', title = ''):
+    def plot_histogram(self, y_axis, x_label='', y_label='', title=''):
         fig = plt.figure()
-        ax = fig.add_subplot(1,1,1)
-        plt.setp(ax.get_xticklabels(), rotation=15)
-        plt.hist(y_axis, bins = 'auto')
+        ax = fig.add_subplot(1, 1, 1)
+        self.set_plot_style(x_label, y_label, title)
+
+        result = plt.hist(y_axis, bins='fd', color='blue',
+                          edgecolor='black', alpha=.75)
+        plt.show()
+        return result
+
+    def set_plot_style(self, x_label, y_label, title):
+        plt.xlabel(x_label)
         plt.ylabel(y_label)
         plt.title(title)
-        plt.show()
